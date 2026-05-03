4ae16c84e0e5ec6652d763ba935d47de17767187
evamomanyi <evanyaks13@gmail.com>
2026-05-03 10:09:27 +0300

    completed lab

--- index.js
+++ index.js
@@ -4,3 +4,21 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+require('datejs');
+
+const combinedObject = []
+
+function combineUsers(...args) {
+ const combinedObject = {
+  users: [],
+  merge_date: '5/3/2026'
+ }
+ 
+args.forEach(array => {
+  combinedObject.users = [...combinedObject.users, ...array]
+})
+combinedObject.merge_date = new Date().toString('5/3/2026');
+
+return combinedObject;
+}
